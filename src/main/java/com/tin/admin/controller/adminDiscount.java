53fd93e8ec6d7097876cdf998027eaebdd477e38
ThuanFpoly <thuanlvps18911@fpt.edu.vn>
2022-11-09 17:17:38 +0700

    multi language

--- src/main/java/com/tin/admin/controller/adminDiscount.java
+++ src/main/java/com/tin/admin/controller/adminDiscount.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class adminDiscount {
 
-
 	@RequestMapping("/admin/discounts")
 	public String adminDiscount(Model model) {
 		return "admin/Discount/discounts";
